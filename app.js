c4ec1b26998f1fd7f3b0d826e9002094ec4036bc
Progress <ezeamakachisom2002@gmail.com>
2022-11-29 10:48:29 +0100

    Add  remove and add function to app.js

--- app.js
+++ app.js
@@ -1,25 +1,71 @@
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const myButtton = document.querySelector('.add-button');
-const list = document.querySelector('.container ul');
-myButtton.addEventListener('click', () => {
-  if(title.value || author.value !== '') {
-   e.preventDefault();
-    const myLi = document.createElement('li');
-    myLi.innerHTML = title.value + ' by ' + author.value;
-    list.appendChild(myLi);
+let books = [];
 
-    const mySpan = document.createElement('span');
-    mySpan.innerHTML = 'X';
-    myLi.appendChild(mySpan);
-  }
-  const close = document.querySelectorAll('span');
-  for (let i = 0; i < close.length; i++) {
-    close[i].addEventListener('click', () => {
-  
-      close[i].parentElement.remove();
+const awesomeBooks = document.getElementById('awesomeBooks');
+
+function updateLocalstorage() {
+  localStorage.setItem('bookLibrary', JSON.stringify(books));
+}
+
+function remove(id) {
+  books = books.filter((book) => book.id !== id);
+  updateLocalstorage();
+}
+
+function removeDom(element) {
+  element.querySelectorAll('.btn').forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      const parent = e.target.parentNode;
+      remove(parent.id);
+      parent.remove();
     });
-    }
-  title.value = '';
+  });
+}
+
+function render(book) {
+  awesomeBooks.innerHTML += `
+      <li id="${book.id}">
+        <h3>${book.title}</h3>
+        <h4>${book.author}</h4>
+        <button class="btn">remove</button>
+        <hr>  
+      </li>
+    
+      `;
+  removeDom(awesomeBooks);
+}
+
+function add(book) {
+  render(book);
+  books.push(book);
+  removeDom(awesomeBooks);
+  updateLocalstorage();
+}
+
+document.querySelector('form').onsubmit = (e) => {
+  e.preventDefault();
+  const error = document.getElementById('error');
+  const { title, author } = e.target;
+  if (title.value.length < 1 || author.value.length < 1) {
+    error.innerHTML = 'input filed must not be empty';
+    setTimeout(() => {
+      error.innerHTML = '';
+    }, 3000);
+  } else {
+    error.innerHTML = '';
+    add({
+      id: Date.now().toString(),
+      title: title.value,
+      author: author.value,
+    });
+    title.value = '';
     author.value = '';
-});
+  }
+};
+
+// if (localStorage.getItem('bookLibrary')) {
+//   books = JSON.parse(localStorage.getItem('bookLibrary'));
+// } else {
+//   localStorage.setItem('bookLibrary', JSON.stringify([]));
+// }
+
+// books.forEach((book) => render(book));
